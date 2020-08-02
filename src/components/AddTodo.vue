7f47ee66c99adfff14025f022d9837c58a4e6104
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-03 11:13:07 +1200

    create a form with inputs type text to add todos and type submit to submit the data

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,13 +1,15 @@
 <template>
-    <div></div>
+  <div>
+    <form>
+      <input type="text" name="title" placeholder="Add Todo..." />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
 </style>
