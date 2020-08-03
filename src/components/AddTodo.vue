a8885699abd2013b4fa5165899a8d1b7a86a23b7
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-03 12:33:19 +1200

    create a method to add todos, which creates an object including id, title and the completed status and emit it to the parent

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
     <form @submit="addTodo">
-      <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
+      <input
+        type="text"
+        v-model="title"
+        name="title"
+        placeholder="Add Todo..."
+      />
       <input type="submit" value="Submit" class="btn" />
     </form>
   </div>
 </template>
 
 <script>
+import uuid from 'uuid';
 export default {
-  name: "AddTodo",
+  name: 'AddTodo',
   data() {
     return {
-      title: "",
+      title: '',
     };
   },
-  methods:{
-      addTodo(){
-          const newTodo ={
-              id: ''
-          }
-      }
-  }
+  methods: {
+    addTodo() {
+      const newTodo = {
+        id: uuid.v4(),
+        title: this.title,
+        completed: false,
+      };
+      //Send up to parent
+      this.$emit('add-todo', newTodo);
+    },
+  },
 };
 </script>
 
@@ -29,11 +39,11 @@ export default {
 form {
   display: flex;
 }
-input [type="text"] {
+input [type='text'] {
   flex: 10;
   padding: 5px;
 }
-input [type="submit"] {
+input [type='submit'] {
   flex: 2;
 }
 </style>
