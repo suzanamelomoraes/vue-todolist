ae9d9d6abee404719e731f5ffb6fc88cfd293733
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-03 14:02:26 +1200

    include created() to call http

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'App',
@@ -20,23 +21,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true,
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   },
   methods: {
@@ -47,6 +32,8 @@ export default {
       this.todos = [...this.todos, newTodo];
     },
   },
+
+  created() {},
 };
 </script>
 
